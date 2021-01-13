8b875df60e4b6016372d7f8146bb78fc5fa74096
Paul Velasco <paul.velasco.code@gmail.com>
2021-01-13 10:48:54 -0500

    3er

--- app/src/main/java/com/app/webservice/MainActivity.java
+++ app/src/main/java/com/app/webservice/MainActivity.java
@@ -1,24 +1,30 @@
 package com.app.webservice;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.app.webservice.fragments.BienvenidaFragment;
+import com.app.webservice.fragments.ConsultaListaUsuarioImagenUrlFragment;
+import com.app.webservice.fragments.ConsultaUsuarioUrlFragment;
+import com.app.webservice.fragments.ConsultarListaUsuariosFragment;
+import com.app.webservice.fragments.ConsultarUsuarioFragment;
+import com.app.webservice.fragments.ConsutarListausuarioImagenFragment;
+import com.app.webservice.fragments.DesarrolladorFragment;
+import com.app.webservice.fragments.RegistrarUsuarioFragment;
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private AppBarConfiguration mAppBarConfiguration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,25 +32,85 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        Fragment miFragment=new BienvenidaFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_main,miFragment).commit();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        Fragment miFragment=null;
+        boolean fragmentSeleccionado=false;
+
+        if (id == R.id.nav_inicio) {
+            miFragment=new BienvenidaFragment();
+            fragmentSeleccionado=true;
+        }else if (id == R.id.nav_registro) {
+            miFragment=new RegistrarUsuarioFragment();
+            fragmentSeleccionado=true;
+        } else if (id == R.id.nav_consulta_individual) {
+            miFragment=new ConsultarUsuarioFragment();
+            fragmentSeleccionado=true;
+        } else if (id == R.id.nav_consulta_Url) {
+            miFragment=new ConsultaUsuarioUrlFragment();
+            fragmentSeleccionado=true;
+        } else if (id == R.id.nav_consulta_gral) {
+            miFragment=new ConsultarListaUsuariosFragment();
+            fragmentSeleccionado=true;
+        } else if (id == R.id.nav_consulta_gral_img) {
+            miFragment=new ConsutarListausuarioImagenFragment();
+            fragmentSeleccionado=true;
+        }else if (id == R.id.nav_consulta_gral_img_url) {
+            miFragment=new ConsultaListaUsuarioImagenUrlFragment();
+            fragmentSeleccionado=true;
+        } else if (id == R.id.nav_desarrollador) {
+            miFragment=new DesarrolladorFragment();
+            fragmentSeleccionado=true;
+        }
+
+        if (fragmentSeleccionado==true){
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_main,miFragment).commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
@@ -52,12 +118,5 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
